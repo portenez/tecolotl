bf85b4cf144aae5ce36ed817293cd0c10f2548af
vgarcia <vgarcia@microstrategy.com>
2012-10-02 15:57:01 -0400

    first commit

---
+++ groovy-utils/src/roundBehavior.groovy
@@ -0,0 +1,15 @@
+totalUsers = 255
+divideIn = 4
+percentage = 1/divideIn
+notRounded = totalUsers*percentage
+//users = Math.ceil(notRounded)
+//users = Math.floor(notRounded)
+users = Math.round(notRounded)
+generatedTotal = users*divideIn
+toPrint =  ['total':totalUsers, 
+'percentage':percentage,
+ 'not-roudned':notRounded, 
+ 'rounded':users,
+ 'total': generatedTotal]
+
+println toPrint 
