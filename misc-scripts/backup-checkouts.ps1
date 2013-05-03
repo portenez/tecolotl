86156dff01bf14eb3f0a1ec9c48ecd103b16bcb2
Victor Garcia <vh.garcia.valencia@gmail.com>
2013-05-03 18:45:03 -0400

    added some comments

--- misc-scripts/backup-checkouts.ps1
+++ misc-scripts/backup-checkouts.ps1
@@ -1,7 +1,16 @@
 #2013.05.04 - backup checkouts
+#Victor Garcia
+#
+#This script needs to be run from within a VIEW/VOB.
+#I tested it on a snapshot view, never tried on a dynamic view.
+#
+#Run this script from the folder you want to backup. For now the 
+#target for the backup is hardcoded
+#
 cleartool lsco -s -r -me -cview |
 	Foreach {
 		$oldName = $_
+		#TODO pass in the backup target as an argument to the script
 		$newName = $oldName -replace '^\.\\','e:\checkout-backup\'
 		$oldFile = Get-Item $oldName 
 		if(! $oldFile.PSIsContainer){
