0031544322a94a48b0811abc09307582a8f0a425
Victor Garcia <vh.garcia.valencia@gmail.com>
2013-12-30 14:34:47 -0500

    copying the wars and jars now work

--- misc-scripts/copyWars.ps1
+++ misc-scripts/copyWars.ps1
@@ -3,14 +3,21 @@ $rootFolder = "E:\views\vgarcia_cloud_enterprise\Cloud_Enterprise\license-manage
 $pattern = "license-manager.+?"
 $patternForWebapps = "license-manager-resource.war"
 $patternForLifeRay = "license-manager.+?\.war"
+$lifeRayPath = "E:\liferay-portal-6.2.0-ce-ga1"
+$tomcatFolder = gci -Path $lifeRayPath -recurse | where {$_.PSIsContainer -and $_.Name -match ".*?tomcat.*?"}
+
+echo "Tomcat liferayFolder " $tomcatFolder.Name
+
 gci -Path $rootFolder -recurse | 
 	where {$_.Name -match $pattern -and !$_.PSIsContainer} |
 	% {
 		if($_.Name -match $patternForWebapps){
 			echo "to webapps" $_.Name
+			cpi $_.FullName "$($tomcatFolder.FullName)/webapps"
 		}
 		if($_.Name -match $patternForLifeRay){
 			echo "to-lifeRay" $_.Name 
+			cpi $_.FullName $lifeRayPath/deploy
 		}
 	}
 	
