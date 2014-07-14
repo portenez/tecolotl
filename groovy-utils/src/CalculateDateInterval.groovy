84ff3fa2221b9fc78b95722ceb07406aa63c52b4
Victor Garcia <vgarcia@microstrategy.com>
2014-07-14 15:32:19 -0400

    added functionality for min/max

--- groovy-utils/src/CalculateDateInterval.groovy
+++ groovy-utils/src/CalculateDateInterval.groovy
@@ -11,34 +11,60 @@ import java.text.SimpleDateFormat
 def srcFile = new File("E:\\to-delete\\status.xml")
 def datePattern = ~/\d{4}-\d{2}-\d{2}\s+\d{2}:\d{2}:\d{2}/
 def dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+def dayPattern = ~/\d{4}-\d{2}-\d{2}/
 
 println "processing file: ${srcFile.canonicalPath}"
 
-def foundDates = []
+def foundDates = [:]
 
 srcFile.eachLine{line ->
 	
-//        println "${line}"
-				
         def gline = "${line}"
 				def allDates = gline.findAll(datePattern)
 				
 				allDates.each{ foundDate -> 
-//					println foundDate;
-					foundDates << dateFormat.parse(foundDate)
+					
+					def day = foundDate.find(dayPattern)
+					
+					if(foundDates[day]== null){
+						foundDates[day] = [:]
+						foundDates[day].points = []
+					}
+					
+					def curDay = foundDates[day]
+					def curTime = dateFormat.parse(foundDate)
+					
+					curDay.points << curTime
+					
+					if(curDay.min == null || curTime.time < curDay.min.time){
+						curDay.min = curTime
+					}
+					if(curDay.max == null ||  curDay.max.time < curTime.time){
+						curDay.max = curTime
+					}
+					
 				}
 }
 
-println foundDates
 
-def diffs = []
-def prev = null;
-
-foundDates.each { 
-	if(prev != null){
-		diffs << it.time - prev.time 
+foundDates.each{key, day ->
+	
+	def prev = null
+	
+	day.points.each {
+		
+		if(day.diffs == null){
+			day.diffs = []
+		}
+		
+		if(prev != null){
+			day.diffs << it.time - prev.time
+		}
+		prev = it
 	}
-	prev = it;
+	
+	println "$key ${day.min} ${day.max}"
 }
 
-println diffs
+
+
