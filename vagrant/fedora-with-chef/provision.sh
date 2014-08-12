81c0a902552aaeab9c9b34266a139ca942fb5c02
portenez <vh.garcia.valencia@gmail.com>
2014-08-11 22:19:52 -0400

    initial commit of vagrant related files

---
+++ vagrant/fedora-with-chef/provision.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+yum -y install ruby-devel gcc gcc-c++ libxml2-devel libxslt-devel autoconf
+gem install chef --no-ri --no-rdoc
+gem install knife-solo --no-ri --no-rdoc
+gem install berkshelf --no-ri --no-rdoc
+gem install foodcritic --no-ri --no-rdoc
