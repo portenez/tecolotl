1c58f60c9b89e395e41fdf4eb30689c99db0c974
portenez <vh.garcia.valencia@gmail.com>
2014-08-11 22:28:13 -0400

    added git ignore

--- vagrant/fedora-with-chef/provision.sh
+++ vagrant/fedora-with-chef/provision.sh
@@ -2,5 +2,5 @@
 yum -y install ruby-devel gcc gcc-c++ libxml2-devel libxslt-devel autoconf
 gem install chef --no-ri --no-rdoc
 gem install knife-solo --no-ri --no-rdoc
-gem install berkshelf --no-ri --no-rdoc
+#gem install berkshelf --no-ri --no-rdoc
 gem install foodcritic --no-ri --no-rdoc
